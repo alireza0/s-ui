b3fea325d9c153db521a8ec44d39341faf585205
lllrrr2 <61368564+lllrrr2@users.noreply.github.com>
2024-02-21 10:48:16 +0800

    Update index.ts

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,7 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import en from './en'
 import fa from './fa'
-
+import cn from './cn'
 
 export const i18n = createI18n({
   legacy: false,
@@ -10,10 +10,12 @@ export const i18n = createI18n({
   messages: {
     en,
     fa,
+    cn,
   },
 })
 
 export const languages = [
   { title: 'English', value: 'en' },
   { title: 'فارسی', value: 'fa' },
+  { title: '简体中文', value: 'cn' },
 ]
