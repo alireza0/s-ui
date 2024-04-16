68ed8d120f6395f7b1033a5aa2a266459a09d436
Alireza Ahmadi <alireza7@gmail.com>
2024-04-16 14:38:17 +0200

    Add Vietnamese Lang #62
    Co-Authored-By:  vuong2023 <124447749+vuong2023@users.noreply.github.com>

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import en from './en'
 import fa from './fa'
+import vi from './vi'
 import zhcn from './zhcn'
 import zhtw from './zhtw'
 
@@ -11,6 +12,7 @@ export const i18n = createI18n({
   messages: {
     en,
     fa,
+    vi,
     zhcn,
     zhtw
   },
@@ -19,6 +21,7 @@ export const i18n = createI18n({
 export const languages = [
   { title: 'English', value: 'en' },
   { title: 'فارسی', value: 'fa' },
+  { title: 'Tiếng Việt', value: 'vi' },
   { title: '简体中文', value: 'zhcn' },
   { title: '繁體中文', value: 'zhtw' },
 ]
