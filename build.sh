4649bd42aebbfc6c54407bd01094142bb51a784a
tinybug <43219257+tinybug-m@users.noreply.github.com>
2024-11-17 15:20:26 +0330

    fix build problem (#347)
    * fix build problem
    
    * fix fully remove S-UI panel in README.md
    
    * fix frontend build problem (npm install before build frontend)
    
    * web/html folder dosent exist at the fisrt place

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 cd frontend
+npm i
 npm run build
 
 cd ..
 cd backend
 echo "Backend"
 
+mkdir -p web/html
 rm -fr web/html/*
-cp -R ../frontend/dist/ web/html/
+cp -R ../frontend/dist/* web/html/
 
 go build -o ../sui main.go
