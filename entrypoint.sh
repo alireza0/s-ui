331fedc5eea33170027598477f2078b0d88f658f
Ali Rajabpour Sanati <ali.poursanati@gmail.com>
2025-04-02 22:18:06 +0330

    Improved docker feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-./sui migrate
-./sui
+# Set default values if environment variables are not set
+PANEL_PORT=${PANEL_PORT:-2095}
+PANEL_PATH=${PANEL_PATH:-/app}
+SUB_PORT=${SUB_PORT:-2096}
+SUB_PATH=${SUB_PATH:-/sub}
+
+# Apply settings
+/app/sui setting \
+  -port ${PANEL_PORT} \
+  -path ${PANEL_PATH} \
+  -subPort ${SUB_PORT} \
+  -subPath ${SUB_PATH}
+
+# Start the service
+exec /app/sui
